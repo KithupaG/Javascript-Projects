94143982616e70c181a9fce0a2aaa97ba8bdf029
Kithupa Gajanayake <gkithupa@gmail.com>
2025-09-28 00:30:23 +0530

    Create index.js
    Added counter program

---
+++ counter/js/index.js
@@ -0,0 +1,13 @@
+document.getElementById("number");
+
+function increase() {
+    document.getElementById("number").innerText++;   
+}
+
+function decrease() {
+    document.getElementById("number").innerText--;
+}
+
+function reset() {
+    document.getElementById("number").innerText = 0;
+}
