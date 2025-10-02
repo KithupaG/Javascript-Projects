55e427cf28575230d95869d2e17ca23badff4937
Kithupa Gajanayake <gkithupa@gmail.com>
2025-10-03 03:10:00 +0530

    Create index.js

---
+++ pokemon_datafetch/js/index.js
@@ -0,0 +1,21 @@
+async function fetchData() {
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok) {
+            throw new Error("Pokemon not found");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+
+        const imgElement = document.getElementById("pokemonSpirte");
+        imgElement.src = pokemonSprite;
+
+        imgElement.style.display = "block";
+        
+    }catch(error) {
+        console.log(error);
+    }
+}
